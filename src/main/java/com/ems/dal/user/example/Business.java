0a64c0999520c643e25945aded5356a6918c00e0
963123564 <963123564@qq.com>
2021-04-05 23:33:29 +0800

    first commit

---
+++ src/main/java/com/ems/dal/user/example/Business.java
@@ -0,0 +1,58 @@
+package com.ems.dal.user.example;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Business implements Serializable {
+    private Long id;
+
+    private Long userId;
+
+    private Integer fraction;
+
+    private Integer businessCount;
+
+    private Date updateTime;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Integer getFraction() {
+        return fraction;
+    }
+
+    public void setFraction(Integer fraction) {
+        this.fraction = fraction;
+    }
+
+    public Integer getBusinessCount() {
+        return businessCount;
+    }
+
+    public void setBusinessCount(Integer businessCount) {
+        this.businessCount = businessCount;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
---
+++ src/main/java/com/ems/dal/user/mapper/NotifyMapper.java
@@ -0,0 +1,36 @@
+package com.ems.dal.user.mapper;
+
+import com.ems.dal.user.example.Notify;
+import com.ems.dal.user.example.NotifyExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface NotifyMapper {
+    int countByExample(NotifyExample example);
+
+    int deleteByExample(NotifyExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Notify record);
+
+    int insertSelective(Notify record);
+
+    List<Notify> selectByExampleWithBLOBs(NotifyExample example);
+
+    List<Notify> selectByExample(NotifyExample example);
+
+    Notify selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Notify record, @Param("example") NotifyExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Notify record, @Param("example") NotifyExample example);
+
+    int updateByExample(@Param("record") Notify record, @Param("example") NotifyExample example);
+
+    int updateByPrimaryKeySelective(Notify record);
+
+    int updateByPrimaryKeyWithBLOBs(Notify record);
+
+    int updateByPrimaryKey(Notify record);
+}
